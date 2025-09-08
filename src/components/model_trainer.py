a7c8ed5e04e06359f7e9c75e42b55bc9ae334b37
ayookuns <ayookuns830@gmail.com>
2025-09-08 19:11:09 +0100

    Model Trainer Completed

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -41,8 +41,45 @@ class ModelTrainer:
                     'DecisionTree Classifier': DecisionTreeClassifier()
                         }
             
+            params = {
+                        "K-Neigbors Classifier": {
+                            'n_neighbors': [3, 5, 7, 9, 11],
+                            'weights': ['uniform', 'distance'],
+                            'p': [1, 2]  # 1: Manhattan, 2: Euclidean
+                        },
+                        "HistGradientBoosting Classifier": {
+                            'learning_rate': [0.01, 0.05, 0.1],
+                            'max_depth': [3, 5, 7, None],
+                            'max_iter': [50, 100, 200],
+                            'min_samples_leaf': [10, 20, 30]
+                        },
+                        "GradientBoosting Classifier": {
+                            'learning_rate': [0.01, 0.05, 0.1],
+                            'n_estimators': [50, 100, 200],
+                            'max_depth': [3, 5, 7],
+                            'subsample': [0.7, 0.8, 0.9]
+                        },
+                        "AdaBoost Classifier": {
+                            'n_estimators': [50, 100, 200],
+                            'learning_rate': [0.01, 0.05, 0.1, 1.0]
+                        },
+                        "RandomForest Classifier": {
+                            'n_estimators': [50, 100, 200],
+                            'criterion': ['gini', 'entropy'],
+                            'max_depth': [None, 5, 10, 20],
+                            'max_features': ['sqrt', 'log2']
+                        },
+                        "DecisionTree Classifier": {
+                            'criterion': ['gini', 'entropy'],
+                            'splitter': ['best', 'random'],
+                            'max_depth': [None, 5, 10, 20],
+                            'max_features': ['sqrt', 'log2']
+                        }
+                    }
+
+            
             model_report:dict = evaluate_models(X_train=X_train, y_train=y_train, X_test =X_test, 
-                                                y_test  = y_test, models  =models)
+                                                y_test  = y_test, models  =models, params = params)
             
              ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import pickle
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -21,14 +22,20 @@ def save_object(file_path, obj):
     
 
 
-def evaluate_models(X_train, y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train,X_test,y_test,models, params):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            
+            para=params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
+
 
             #model.fit(X_train, y_train)  # Train model
 
